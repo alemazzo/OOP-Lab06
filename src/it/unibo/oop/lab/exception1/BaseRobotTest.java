42d30b4b56514729e96324075d4dd56ebf3e4c27
Alessandro Mazzoli <alemazzoli97@gmail.com>
2020-11-03 18:02:07 +0100

    Completed exception1

--- src/it/unibo/oop/lab/exception1/BaseRobotTest.java
+++ src/it/unibo/oop/lab/exception1/BaseRobotTest.java
@@ -2,6 +2,9 @@ package it.unibo.oop.lab.exception1;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+
 import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
@@ -35,10 +38,21 @@ public final class BaseRobotTest {
          */
         for (int i = 0; i < RobotEnvironment.WORLD_X_UPPER_LIMIT; i++) {
             // check if position if coherent
-            assertTrue("[CHECKING MOVING RIGHT]", r1.moveRight());
+            try{
+            	r1.moveRight();
+            } catch (Exception e) {
+            	Assert.fail();
+            }
         }
         // reached the right limit of the world
-        assertFalse("[CHECKING MOVING RIGHT]", r1.moveRight());
+        try{
+        	r1.moveRight();
+        	Assert.fail();
+        } catch (Exception e) {
+        	Assert.assertNotNull(e.toString());
+        }
+        //assertFalse("[CHECKING MOVING RIGHT]", r1.moveRight());
+        
         // checking positions x=50; y=0
         assertEquals("[MOVING RIGHT ROBOT POS X]", RobotEnvironment.WORLD_X_UPPER_LIMIT, r1.getEnvironment().getCurrPosX());
         assertEquals("[MOVING RIGHT ROBOT POS Y]", 0, r1.getEnvironment().getCurrPosY());
@@ -47,10 +61,19 @@ public final class BaseRobotTest {
          */
         for (int i = 0; i < RobotEnvironment.WORLD_Y_UPPER_LIMIT; i++) {
             // check if position if coherent
-            assertTrue("[CHECKING MOVING UP]", r1.moveUp());
+        	try{
+            	r1.moveUp();
+            } catch (Exception e) {
+            	Assert.fail();
+            }
         }
         // reached the upper limit of the world
-        assertFalse("[CHECKING MOVING UP]", r1.moveUp());
+        try{
+        	r1.moveUp();
+        	Assert.fail();
+        } catch (Exception e) {
+        	Assert.assertNotNull(e.toString());
+        }
         // checking positions x=50; y=80
         assertEquals("[MOVING RIGHT ROBOT POS X]", RobotEnvironment.WORLD_X_UPPER_LIMIT, r1.getEnvironment().getCurrPosX());
         assertEquals("[MOVING RIGHT ROBOT POS Y]", RobotEnvironment.WORLD_Y_UPPER_LIMIT, r1.getEnvironment().getCurrPosY());
@@ -78,7 +101,12 @@ public final class BaseRobotTest {
         // verify position: same as start position
         assertEquals("[CHECKING ROBOT INIT POS Y]", 0, r2.getEnvironment().getCurrPosY());
         // out of world: returns false
-        assertFalse("[CHECKING MOVING UP]", r2.moveUp());
+        try{
+        	r2.moveUp();
+        	Assert.fail();
+        } catch (Exception e) {
+        	Assert.assertNotNull(e.toString());
+        }
         // recharge battery
         r2.recharge();
         // verify battery level
